1d91de1a7acf9fe2603b34afa105c67f2058f033
mhmtclk1705 <mehmetcelik1705@gmail.com>
2022-05-02 11:28:57 +0300

    firebase

--- src/utils/firebaseUtil.js
+++ src/utils/firebaseUtil.js
@@ -9,13 +9,19 @@ import {
 } from "firebase/auth";
 
 const app = initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_apiKey,
-  authDomain: process.env.REACT_APP_FIREBASE_authDomain,
-  databaseURL: process.env.REACT_APP_FIREBASE_databaseURL,
-  projectId: process.env.REACT_APP_FIREBASE_projectId,
-  storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
-  appId: process.env.REACT_APP_FIREBASE_appId,
+  // apiKey: process.env.REACT_APP_FIREBASE_apiKey,
+  // authDomain: process.env.REACT_APP_FIREBASE_authDomain,
+  // databaseURL: process.env.REACT_APP_FIREBASE_databaseURL,
+  // projectId: process.env.REACT_APP_FIREBASE_projectId,
+  // storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
+  // appId: process.env.REACT_APP_FIREBASE_appId,
+  apiKey: "AIzaSyCLLZ2cgpW_9Z0dAn0IGrcz13SjxAx0tRc",
+  authDomain: "mhmtclk-reduxthunk.firebaseapp.com",
+  projectId: "mhmtclk-reduxthunk",
+  storageBucket: "mhmtclk-reduxthunk.appspot.com",
+  messagingSenderId: "246743130668",
+  appId: "1:246743130668:web:11db3954a3e15dd87b6789"
 });
 
 export const auth = getAuth(app);
