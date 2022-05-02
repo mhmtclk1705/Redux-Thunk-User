db31344c1f4663301cbd581ed0deca41b9b5ddf6
mhmtclk1705 <mehmetcelik1705@gmail.com>
2022-05-02 11:46:23 +0300

    newsapi

--- src/redux/thunk/newsThunk.js
+++ src/redux/thunk/newsThunk.js
@@ -2,35 +2,34 @@ import axios from "axios";
 import { setLoading, clearLoading } from "../actions/appActions";
 import { setNewsList } from "../actions/newsActions";
 
-
 const url =
-"https://newsapi.org/v2/everything?" +
-"q=apple&" +
-"from=2022-05-01&" +
-"to=2022-05-01&" +
-"sortBy=popularity&" +
-"apiKey=62394115e9ff4a768ce424837b911c2b";
+  "https://newsapi.org/v2/everything?q=tesla&from=2022-04-02&sortBy=publishedAt&apiKey=62394115e9ff4a768ce424837b911c2b";
+// "https://newsapi.org/v2/everything?" +
+// "q=apple&" +
+// "from=2022-05-01&" +
+// "to=2022-05-01&" +
+// "sortBy=popularity&" +
+// "apiKey=62394115e9ff4a768ce424837b911c2b";
 
-//! getNews fonksiyonu başka bir fonksiyonu döndürüyor. 
+//! getNews fonksiyonu başka bir fonksiyonu döndürüyor.
 //! Bu durumda çağırırken dispatch(getNews()) şeklinde kullanmak gerekir.
 
 export const getNews = () => {
-    return async (dispatch) => {
-try {
-  dispatch(setLoading());
-  const { data } = await axios(url);
-  // console.log(data.articles);
-  dispatch(setNewsList(data.articles));
-} catch (error) {
-  console.log(error);
-} finally {
-  dispatch(clearLoading());
-}
+  return async (dispatch) => {
+    try {
+      dispatch(setLoading());
+      const { data } = await axios(url);
+      // console.log(data.articles);
+      dispatch(setNewsList(data.articles));
+    } catch (error) {
+      console.log(error);
+    } finally {
+      dispatch(clearLoading());
     }
+  };
 };
 
-
-// ! bu kullanımda getNews bir değişken gibi düşünülebilir 
+// ! bu kullanımda getNews bir değişken gibi düşünülebilir
 //! News tarafında dispatch(getNews) şeklinde çağırılır...
 
 // export const getNews = async (dispatch) => {
